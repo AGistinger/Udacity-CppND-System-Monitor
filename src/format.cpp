918e408213bb069f26d583dd263b0ba6393dee4b
Adrianne <54363707+Nyrathalnian@users.noreply.github.com>
2019-11-04 12:48:54 -0500

    Updated format to handle adding 0 in front of <10

--- src/format.cpp
+++ src/format.cpp
@@ -11,14 +11,29 @@ using std::string;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) 
 { 
-    int time = seconds;
-    int hour, min;
+  int time = seconds;
+  int hour, min;
+  string time_seconds;
 
-    hour = time / 3600;
-    time = time & 3600;
-    min = time / 60;
-    time = time % 60;
-
-    string time_seconds = std::to_string(hour) + ":" + std::to_string(min) + ":" + std::to_string(time);
-    return time_seconds; 
+  hour = time / 3600;
+  time = time & 3600;
+  min = time / 60;
+  time = time % 60;
+  
+  if(hour < 10)
+  {
+    time_seconds = "0" + std::to_string(hour) + ":" + std::to_string(min) + ":" + std::to_string(time);
+  }
+  else if(min < 10)
+  {
+    time_seconds = std::to_string(hour) + ":" + "0" + std::to_string(min) + ":" + std::to_string(time);
+  }
+  else if(time < 10)
+  {
+    time_seconds = std::to_string(hour) + ":" + std::to_string(min) + ":" + "0" + std::to_string(time);
+  }
+  else
+  time_seconds = std::to_string(hour) + ":" + std::to_string(min) + ":" + std::to_string(time);
+  
+  return time_seconds; 
 }
